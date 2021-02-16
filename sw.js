d3da8b898229657bddf1bb1eb3e6335743349e0c
wdm1732418365 <1732418365@qq.com>
2021-02-16 15:46:47 +0800

    Site updated: 2021-02-16 15:46:45

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","fa3983bf8e3d82d18bef59eee1b0ea09"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
